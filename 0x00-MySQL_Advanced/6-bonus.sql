1ec1113795656d55c6a298e5f8e86f824d370215
nathan-assefa <aasamnew@gmail.com>
2023-08-10 06:41:31 +0000

    0x00-MySQL_Advanced

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -11,13 +11,10 @@ BEGIN
     -- Declare a variable to store the project ID
     DECLARE project_id INT;
 
-    -- Check if the project already exists in the projects table
-    SELECT id INTO project_id FROM projects WHERE name = project_name;
-
-    -- If no project found, insert it and retrieve the generated ID
-    IF project_id IS NULL THEN
+    IF (SELECT COUNT(*) FROM projects WHERE name = project_name) = 0
+    THEN
         INSERT INTO projects (name) VALUES (project_name);
-    END IF;
+    END IF;    
 
     SET project_id = (SELECT id FROM projects WHERE name = project_name LIMIT 1);
     -- Insert the correction record
