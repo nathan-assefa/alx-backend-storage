2f7847089014a42c33b01795fc4394f86fec202a
nathan-assefa <aasamnew@gmail.com>
2023-08-10 06:39:50 +0000

    0x00-MySQL_Advanced

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -3,17 +3,25 @@
 DROP PROCEDURE IF EXISTS AddBonus;
 DELIMITER //
 CREATE PROCEDURE AddBonus(
-    IN user_id INT,
-    IN project_name VARCHAR(255),
-    IN score FLOAT)
+	IN user_id INT,
+	IN project_name VARCHAR(255),
+	IN score FLOAT
+)
 BEGIN
+    -- Declare a variable to store the project ID
     DECLARE project_id INT;
-    IF (SELECT COUNT(*) FROM projects WHERE name = project_name) = 0
-    THEN
+
+    -- Check if the project already exists in the projects table
+    SELECT id INTO project_id FROM projects WHERE name = project_name;
+
+    -- If no project found, insert it and retrieve the generated ID
+    IF project_id IS NULL THEN
         INSERT INTO projects (name) VALUES (project_name);
     END IF;
+
     SET project_id = (SELECT id FROM projects WHERE name = project_name LIMIT 1);
-    INSERT INTO corrections (user_id, project_id, score) VALUES(user_id, project_id, score);
-END
+    -- Insert the correction record
+    INSERT INTO corrections (user_id, project_name, score) VALUES (user_id, project_name, score);
+END;
 //
 DELIMITER ;
