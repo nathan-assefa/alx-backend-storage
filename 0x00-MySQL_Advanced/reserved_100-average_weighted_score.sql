501eff8fd2525381bfd9cbe4d0e89929036a0da2
nathan-assefa <aasamnew@gmail.com>
2023-08-11 09:53:30 +0000

    0x00-MySQL_Advanced

--- 0x00-MySQL_Advanced/reserved_100-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/reserved_100-average_weighted_score.sql
@@ -8,11 +8,11 @@ CREATE PROCEDURE ComputeAverageWeightedScoreForUser(
 )
 BEGIN
     DECLARE w_avg_score FLOAT;
-    (SELECT SUM(score * weight) / SUM(weight) INTO w_avg_score
-	FROM users AS U
-        JOIN corrections as C ON U.id=C.user_id
-        JOIN projects AS P ON C.project_id=P.id
-        WHERE U.id=user_id);
+    SET w_avg_score = (SELECT SUM(score * weight) / SUM(weight)
+                        FROM users AS U
+                        JOIN corrections as C ON U.id=C.user_id
+                        JOIN projects AS P ON C.project_id=P.id
+                        WHERE U.id=user_id);
     UPDATE users SET average_score = w_avg_score WHERE id=user_id;
 END
 //
