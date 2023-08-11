51e3bb17935e57ce587ea6701f3435f43bd49747
nathan-assefa <aasamnew@gmail.com>
2023-08-11 06:10:05 +0000

    0x00-MySQL_Advanced

---
+++ 0x00-MySQL_Advanced/bonussss.sql
@@ -0,0 +1,29 @@
+-- implementing stored procedues
+-- updating students score
+-- Let us first drop the stored procedure if exists
+DROP PROCEDURE IF EXISTS AddBonus;
+-- Let us define the delimiter thereafter
+DELIMITER $$
+-- Let us define the stored procedure
+CREATE PROCEDURE AddBonus(
+	IN user_id INT,
+	IN project_name VARCHAR(225),
+	IN score DECIMAL(5, 2)
+)
+-- Here we begin the stored procedure
+BEGIN
+	DECLARE project_id INT;
+	-- Let us then check if the project is already registered
+	SELECT id INTO project_id FROM projects where projects.name = project_name;
+	
+	-- register the project if it is not registered yet
+	IF project_id IS NULL THEN
+		INSERT INTO projects(project_name) VALUES (project_name);
+		-- SET project_id = LAST_INSERT_ID();
+	END IF;
+	SET project_id = (SELECT id FROM projects WHERE name = project_name);
+
+	INSERT INTO corrections(user_id, project_id, score) VALUES (user_id, project_id, score);
+END;
+$$
+DELIMITER ;
