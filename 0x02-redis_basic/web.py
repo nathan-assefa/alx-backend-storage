ffa103f1896f8e19397bf54b286fe10edf3255b9
nathan-assefa <aasamnew@gmail.com>
2023-08-16 15:09:49 +0000

    redis

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -5,17 +5,29 @@ import requests
 
 
 r = redis.Redis()
-count = 0
 
 
 def get_page(url: str) -> str:
-    """ tracking how many times a particular URL was accessed """
-    r.set(f"cached:{url}", count)
+    """Tracking how many times a particular URL was accessed."""
+    
+    # Increment the access count in Redis
+    count_key = f"count:{url}"
+    current_count = r.get(count_key)
+    if current_count is None:
+        current_count = 0
+    else:
+        current_count = int(current_count)
+    new_count = current_count + 1
+    r.set(count_key, new_count)
+    
+    # Fetch the HTML content using requests
     resp = requests.get(url)
-    r.incr(f"count:{url}")
-    r.setex(f"cached:{url}", 10, r.get(f"cached:{url}"))
+    
+    # Cache the content with a 10-second expiration time
+    cache_key = f"cached:{url}"
+    r.setex(cache_key, 10, resp.text)
+    
     return resp.text
-
 
 if __name__ == "__main__":
     get_page('http://slowwly.robertomurray.co.uk')
