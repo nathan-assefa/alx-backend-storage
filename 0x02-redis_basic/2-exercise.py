9a4beb78625e3d18abf94ac56a33976d52ec6f16
nathan-assefa <aasamnew@gmail.com>
2023-08-16 11:36:06 +0000

    redis

--- 0x02-redis_basic/2-exercise.py
+++ 0x02-redis_basic/2-exercise.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import redis
 import uuid
-from typing import Union, Callable, Any
+from typing import Union, Callable
 """ Basic operations of redis """
 
 
@@ -16,15 +16,15 @@ class Cache:
         random_key = str(uuid.uuid4())
         self._redis.set(random_key, data)
         return random_key
-    
-    def get(self, key: str, fn: Callable = None) -> (Any, None):
+
+    def get(self, key: str, fn: Callable = None) -> Union[bytes, None]:
         value = self._redis.get(key)
-        if value and fn:
+        if value is not None and fn is not None:
             return fn(value)
         return value
 
     def get_str(self, key: str) -> Union[str, None]:
-        return self.get(key, lambda value: str(value))
+        return self.get(key, lambda value: value.decode('utf-8'))
 
-    def get_int(self, key: int) -> Union[int, None]:
+    def get_int(self, key: str) -> Union[int, None]:
         return self.get(key, lambda value: int(value))
